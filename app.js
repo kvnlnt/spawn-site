a764f0d4054b64b70bead882b11a8705b8cbc02e
Kevin Lint <kevinlint@toolhouse.com>
2017-10-25 14:05:54 -0700

    cleanup

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const app = require('./app.json');
 const chalk = require('chalk');
 const Clean = require('./tasks/clean');
-const ConvertSpreadsheet = require('./tasks/convert-spreadsheet');
-const Deploy = require('./tasks/deploy');
 const Dev = require('./tasks/dev');
 const Build = require('./tasks/build');
 const fs = require('fs');
@@ -29,13 +27,6 @@ program
     });
 
 program
-    .command('convert <source>')
-    .description('convert json converted csv from spreadsheet')
-    .action(function(source) {
-        new ConvertSpreadsheet(source);
-    });
-
-program
     .command('dev')
     .description('spin up dev server')
     .option("-l, --launch <launch>", "Autolaunch the browser", true)
@@ -54,19 +45,6 @@ program
         new Clean();
         new Build();
     });
-
-program
-    .command('deploy')
-    .description('deploy to test server')
-    .option('-u', '--username <username>', 'server username', /^(test|prod)$/i, 'test')
-    .option('-e, --env <env>', 'deployment environment', /^(test|prod)$/i, 'test')
-    .action(function(options) {
-        console.log(options);
-        // new Clean();
-        // new Build();
-        // new Deploy(options);
-    });
-
 
 // auto run help
 if (!process.argv.slice(2).length) {
